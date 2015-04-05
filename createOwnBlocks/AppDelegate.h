486034a5cd7443c10f6492fde80ac366005970b0
於程鹏 <yuchengpeng@yuchengpengdeMac.local>
2015-04-04 19:29:26 -0700

    Initial Commit

---
+++ createOwnBlocks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  createOwnBlocks
+//
+//  Created by 於程鹏 on 15-4-4.
+//  Copyright (c) 2015年 於程鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ createOwnBlocks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  createOwnBlocks
+//
+//  Created by 於程鹏 on 15-4-4.
+//  Copyright (c) 2015年 於程鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
